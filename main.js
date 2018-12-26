f77c1057e499e53751a86178fad5eb1cecaa7410
jiavu <39624614+jiavu@users.noreply.github.com>
2018-12-27 00:34:28 +0100

    Almost working as intended

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 //vlet testEl = document.getElementById("test2");
 //testEl.innerHTML = "test2";
 
-const colorField = document.getElementById("container");
-// style="background-image: radial-gradient( {{ metrColor }}, rgb(0,0,0,0) );"
+const alphaCh = document.getElementById("alphaCh");
 
-const newBarColor = "255,0,0";	// in rgb()	// (red)
-const beatColor = "0,0,255";	// in rgb()	// (blue)
-let alpha = 1;	// 0 bis 1
+
+const newBarColor = "rgb(255,0,0)";	// in rgb()	// (red)
+let beatColor = "rgb(0,0,255)";	// in rgb()	// (blue)
 
 
 angular.module("metronome", [])
@@ -15,38 +14,35 @@ angular.module("metronome", [])
 		
 		const ctrl = this;
 		
-		$scope.metrColor = `rgb(${newBarColor},${alpha})`;
+		$scope.bgColor = "black";
+		$scope.metrColor = newBarColor;
 		$scope.tempo = 120;
 		$scope.active = false;
 		$scope.changeTo = "Start";
 		
 		ctrl.beat = 1;
 		ctrl.interv = 500;
-		ctrl.timeline = new TimelineLite();
-
 
 		ctrl.activeMetr = function() {
-			//let intervalID;
 
 				if ($scope.active) {
-					ctrl.timeline.set(colorField, {
-						backgroundImage: `radial-gradient( ${$scope.metrColor}, rgb(0,0,0,0) )`
-					}).to(colorField, ctrl.interv/1000, {
-						backgroundImage: "radial-gradient( ${$scope.metrColor}, rgb(0,0,0,0) )",
-						onComplete: ctrl.activeMetr
+					TweenMax.fromTo(alphaCh, ctrl.interv/1000/2, {
+						opacity: 0,
+					},{
+						onRepeat: function() {
+							$scope.metrColor = (ctrl.beat === 1)?
+								newBarColor : beatColor;
+							ctrl.beat++;
+							if (ctrl.beat >= 9) ctrl.beat = 1;
+							console.log(ctrl.beat, $scope.metrColor);
+						},
+						opacity: 1,
+						yoyo: true,
+						repeat: -1,
+						ease: Power0.easeNone
 					});
-
-				/*
-				invervalID = window.setInterval(function(){
-					console.log("executed");
-					for (let f = 0; f < Math.floor(interv); f++) {
-						$scope.metrColor = `rgb(${newBarColor},${alpha-f*1/interv})`;
-						console.log($scope.metrColor);
-					}
-				}, interv);
 			} else {
-				clearInterval(intervalID);
-				*/
+				TweenMax.set(alphaCh, { opacity: 1 });
 			}
 		};
 		
@@ -60,6 +56,9 @@ angular.module("metronome", [])
 			ctrl.activeMetr(ctrl.interv, ctrl.beat);
 		};
 		
-		ctrl.changeBPM = () => ctrl.interv = 1000/($scope.tempo/60);
+		ctrl.changeBPM = () => {
+			ctrl.interv = 1000/($scope.tempo/60);
+			ctrl.activeMetr();
+		};
 
 	});
